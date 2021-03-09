b1c8bbabb8450c978fcab216330822cfa31de2d9
Stephen Buchanan <stephen.buchanan@random.studio>
2021-03-09 13:36:59 +0100

    basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,33 @@ import "./App.scss";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import SimpleTest from "./SimpleTest";
 
-const App: React.FunctionComponent<{}> = () => {
+const App = () => {
   return (
-    <div className="App">
-      <SimpleTest />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/static">
+            <SimpleTest />
+          </Route>
+          <Route path="/transition">TODO: transition prototype</Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
+
+const Home = () => (
+  <div>
+    <p>Click links to view prototypes.</p>
+
+    <nav>
+      <Link to="/static">Simple static test</Link>
+      <Link to="/transition">Transition tests</Link>
+    </nav>
+  </div>
+);
 
 export default App;
