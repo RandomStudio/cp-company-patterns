4443d8883ae207e792dcd18519b9f8eaefd41405
Stephen Buchanan <stephen.buchanan@random.studio>
2021-03-17 12:37:38 +0100

    get dominant colour and palette, display for each item

--- src/Transitions/Item.tsx
+++ src/Transitions/Item.tsx
@@ -1,16 +1,40 @@
+// @ts-ignore
+import ColorThief from "colorthief";
+import { useEffect, useState } from "react";
 interface Props {
   index: number;
   id: number;
   url: string;
 }
 
+interface ColourData {
+  dominantColour?: number[];
+  palette?: number[][];
+}
+
 const Item = (props: Props) => {
+  const colorThief = new ColorThief();
+
+  const [colourData, setColourData] = useState<ColourData>({});
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = props.url;
+    img.onload = async () => {
+      const dominantColour = await colorThief.getColor(img);
+      const palette = await colorThief.getPalette(img);
+      // console.log({ c });
+      setColourData({ dominantColour, palette });
+    };
+  });
+
   return (
     <div className="Item">
       <h2>
         [{props.index}]:#{props.id}
       </h2>
-      <img src={props.url}></img>
+      <img src={props.url} alt="large"></img>
+      <code>{JSON.stringify(colourData)}</code>
       <div>
         <a href="/transition">Back</a>
       </div>
